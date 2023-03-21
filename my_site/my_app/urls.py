ecefcd77ffee60a79283180189f58ed08a352d62
vishesh211 <visheshtiwari211@gmail.com>
2023-03-21 12:22:57 +0530

    created variable and passed it from views to template

--- my_site/my_app/urls.py
+++ my_site/my_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.example_view)
+    path('',views.example_view),
+    path('variable/',views.variable_view),
 ]
--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def example_view(request):
     return render(request,'my_app/example.html')
+
+def variable_view(request):
+    my_var = {
+        'first_name':'Roselin',
+        'last_name':'Franklin',
+        'some_list':[1,2,3],
+        'some_dict':{'inside_key':'inside_value'}
+    }
+    return render(request,'my_app/variable.html',context=my_var)
