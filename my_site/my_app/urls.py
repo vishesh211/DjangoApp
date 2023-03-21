abf3ce3a84cb51caaf772f94c9e6e417007fbe16
vishesh211 <visheshtiwari211@gmail.com>
2023-03-21 18:45:48 +0530

    added tag url in templates

--- my_site/my_app/urls.py
+++ my_site/my_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'my_app'
+
 urlpatterns = [
-    path('',views.example_view),
-    path('variable/',views.variable_view),
+    path('',views.example_view,name='example'),
+    path('variable/',views.variable_view,name='variable'),
 ]
--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -5,10 +5,5 @@ def example_view(request):
     return render(request,'my_app/example.html')
 
 def variable_view(request):
-    my_var = {
-        'first_name':'Roselin',
-        'last_name':'Franklin',
-        'some_list':[1,2,3],
-        'user_logged_in': False
-    }
-    return render(request,'my_app/variable.html',context=my_var)
+    
+    return render(request,'my_app/variable.html')
